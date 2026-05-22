a26224dace23bb3f2dd485521a15a0fcf47f0ee0
HuseyinG21 <boldwicky@gmail.com>
2026-05-22 16:57:27 +0300

    LAB 10 GÜNCEL VERSİYON
    LAB 10 GÜNCEL VERSİYON

--- ardulabyeni.ino
+++ ardulabyeni.ino
@@ -1,238 +1,217 @@
 #include <LiquidCrystal.h>
 
-// LCD
 LiquidCrystal lcd(36, 37, 26, 27, 28, 29);
 
-#define backlight_pin 31
+const int aPin = 22;
+const int bPin = 28;
+const int cPin = 27;
+const int dPin = 26;
+const int ePin = 25;
+const int fPin = 24;
+const int gPin = 23;
 
-// Segment pinleri
-int aPin = 2;
-int bPin = 3;
-int cPin = 4;
-int dPin = 5;
-int ePin = 6;
-int fPin = 7;
-int gPin = 8;
-int dP   = 9;
+const int GND1 = 33;
+const int GND2 = 34;
+const int GND3 = 35;
+const int GND4 = 36;
 
-// Digit GND pinleri
-int dig1Pin = 10;
-int dig2Pin = 11;
-int dig3Pin = 12;
-int dig4Pin = 13;
+const int digitPins[4] = {GND1, GND2, GND3, GND4};
 
-// Counter digitleri
-int dig1 = 0;
-int dig2 = 0;
-int dig3 = 0;
-int dig4 = 0;
+int counter = 0;
+
+unsigned long lastCountTime = 0;
+unsigned long lastMuxTime = 0;
+
+int currentDigit = 0;
+
+void allDigitsOff() {
+ digitalWrite(GND1, LOW);
+ digitalWrite(GND2, LOW);
+ digitalWrite(GND3, LOW);
+ digitalWrite(GND4, LOW);
+}
+
+void clearSegments() {
+ digitalWrite(aPin, LOW);
+ digitalWrite(bPin, LOW);
+ digitalWrite(cPin, LOW);
+ digitalWrite(dPin, LOW);
+ digitalWrite(ePin, LOW);
+ digitalWrite(fPin, LOW);
+ digitalWrite(gPin, LOW);
+}
+
+void showDigit(int n) {
+
+ clearSegments();
+
+ switch (n) {
+
+ case 0:
+ digitalWrite(aPin,HIGH);
+ digitalWrite(cPin,HIGH);
+ digitalWrite(dPin,HIGH);
+ digitalWrite(ePin,HIGH);
+ digitalWrite(fPin,HIGH);
+ digitalWrite(gPin,HIGH);
+ break;
+
+ case 1:
+ digitalWrite(fPin,HIGH);
+ digitalWrite(gPin,HIGH);
+ break;
+
+ case 2:
+ digitalWrite(aPin,HIGH);
+ digitalWrite(bPin,HIGH);
+ digitalWrite(dPin,HIGH);
+ digitalWrite(ePin,HIGH);
+ digitalWrite(gPin,HIGH);
+ break;
+
+ case 3:
+ digitalWrite(aPin,HIGH);
+ digitalWrite(bPin,HIGH);
+ digitalWrite(ePin,HIGH);
+ digitalWrite(fPin,HIGH);
+ digitalWrite(gPin,HIGH);
+ break;
+
+ case 4:
+ digitalWrite(bPin,HIGH);
+ digitalWrite(cPin,HIGH);
+ digitalWrite(fPin,HIGH);
+ digitalWrite(gPin,HIGH);
+ break;
+
+ case 5:
+ digitalWrite(aPin,HIGH);
+ digitalWrite(bPin,HIGH);
+ digitalWrite(cPin,HIGH);
+ digitalWrite(ePin,HIGH);
+ digitalWrite(fPin,HIGH);
+ break;
+
+ case 6:
+ digitalWrite(aPin,HIGH);
+ digitalWrite(bPin,HIGH);
+ digitalWrite(cPin,HIGH);
+ digitalWrite(dPin,HIGH);
+ digitalWrite(ePin,HIGH);
+ digitalWrite(fPin,HIGH);
+ break;
+
+ case 7:
+ digitalWrite(aPin,HIGH);
+ digitalWrite(fPin,HIGH);
+ digitalWrite(gPin,HIGH);
+ break;
+
+ case 8:
+ digitalWrite(aPin,HIGH);
+ digitalWrite(bPin,HIGH);
+ digitalWrite(cPin,HIGH);
+ digitalWrite(dPin,HIGH);
+ digitalWrite(ePin,HIGH);
+ digitalWrite(fPin,HIGH);
+ digitalWrite(gPin,HIGH);
+ break;
+
+ case 9:
+ digitalWrite(aPin,HIGH);
+ digitalWrite(bPin,HIGH);
+ digitalWrite(cPin,HIGH);
+ digitalWrite(ePin,HIGH);
+ digitalWrite(fPin,HIGH);
+ digitalWrite(gPin,HIGH);
+ break;
+
+ }
+
+}
+
+void refreshDisplay() {
+
+ int digits[4] = {
+ counter % 10,
+ (counter / 10) % 10,
+ (counter / 100) % 10,
+ (counter / 1000) % 10
+ };
+
+ allDigitsOff();
+
+ showDigit(digits[currentDigit]);
+
+ digitalWrite(digitPins[currentDigit], HIGH);
+
+ currentDigit++;
+
+ if(currentDigit>=4)
+ currentDigit=0;
+
+}
+
+void updateLCD(){
+
+ lcd.setCursor(0,0);
+ lcd.print("Counter:");
+
+ lcd.setCursor(0,1);
+
+ if(counter<1000) lcd.print("0");
+ if(counter<100) lcd.print("0");
+ if(counter<10) lcd.print("0");
+
+ lcd.print(counter);
+ lcd.print(" ");
+
+}
 
 void setup() {
 
-  // LCD ışığı
-  pinMode(backlight_pin,OUTPUT);
-  analogWrite(backlight_pin,255);
+ pinMode(aPin, OUTPUT);
+ pinMode(bPin, OUTPUT);
+ pinMode(cPin, OUTPUT);
+ pinMode(dPin, OUTPUT);
+ pinMode(ePin, OUTPUT);
+ pinMode(fPin, OUTPUT);
+ pinMode(gPin, OUTPUT);
 
-  // Segment pinleri
-  pinMode(aPin, OUTPUT);
-  pinMode(bPin, OUTPUT);
-  pinMode(cPin, OUTPUT);
-  pinMode(dPin, OUTPUT);
-  pinMode(ePin, OUTPUT);
-  pinMode(fPin, OUTPUT);
-  pinMode(gPin, OUTPUT);
-  pinMode(dP, OUTPUT);
+ pinMode(GND1, OUTPUT);
+ pinMode(GND2, OUTPUT);
+ pinMode(GND3, OUTPUT);
+ pinMode(GND4, OUTPUT);
 
-  // Digit pinleri
-  pinMode(dig1Pin, OUTPUT);
-  pinMode(dig2Pin, OUTPUT);
-  pinMode(dig3Pin, OUTPUT);
-  pinMode(dig4Pin, OUTPUT);
+ lcd.begin(16,2);
 
-  // LCD
-  lcd.begin(16,2);
+ updateLCD();
 
-  lcd.setCursor(0,0);
-  lcd.print("Counter:");
-}
-
-// Sayı gösterme
-void displayNumber(int num){
-
-  switch(num){
-
-    case 0:
-      digitalWrite(aPin, LOW);
-      digitalWrite(bPin, LOW);
-      digitalWrite(cPin, LOW);
-      digitalWrite(dPin, LOW);
-      digitalWrite(ePin, LOW);
-      digitalWrite(fPin, LOW);
-      digitalWrite(gPin, HIGH);
-      break;
-
-    case 1:
-      digitalWrite(aPin, HIGH);
-      digitalWrite(bPin, LOW);
-      digitalWrite(cPin, LOW);
-      digitalWrite(dPin, HIGH);
-      digitalWrite(ePin, HIGH);
-      digitalWrite(fPin, HIGH);
-      digitalWrite(gPin, HIGH);
-      break;
-
-    case 2:
-      digitalWrite(aPin, LOW);
-      digitalWrite(bPin, LOW);
-      digitalWrite(cPin, HIGH);
-      digitalWrite(dPin, LOW);
-      digitalWrite(ePin, LOW);
-      digitalWrite(fPin, HIGH);
-      digitalWrite(gPin, LOW);
-      break;
-
-    case 3:
-      digitalWrite(aPin, LOW);
-      digitalWrite(bPin, LOW);
-      digitalWrite(cPin, LOW);
-      digitalWrite(dPin, LOW);
-      digitalWrite(ePin, HIGH);
-      digitalWrite(fPin, HIGH);
-      digitalWrite(gPin, LOW);
-      break;
-
-    case 4:
-      digitalWrite(aPin, HIGH);
-      digitalWrite(bPin, LOW);
-      digitalWrite(cPin, LOW);
-      digitalWrite(dPin, HIGH);
-      digitalWrite(ePin, HIGH);
-      digitalWrite(fPin, LOW);
-      digitalWrite(gPin, LOW);
-      break;
-
-    case 5:
-      digitalWrite(aPin, LOW);
-      digitalWrite(bPin, HIGH);
-      digitalWrite(cPin, LOW);
-      digitalWrite(dPin, LOW);
-      digitalWrite(ePin, HIGH);
-      digitalWrite(fPin, LOW);
-      digitalWrite(gPin, LOW);
-      break;
-
-    case 6:
-      digitalWrite(aPin, LOW);
-      digitalWrite(bPin, HIGH);
-      digitalWrite(cPin, LOW);
-      digitalWrite(dPin, LOW);
-      digitalWrite(ePin, LOW);
-      digitalWrite(fPin, LOW);
-      digitalWrite(gPin, LOW);
-      break;
-
-    case 7:
-      digitalWrite(aPin, LOW);
-      digitalWrite(bPin, LOW);
-      digitalWrite(cPin, LOW);
-      digitalWrite(dPin, HIGH);
-      digitalWrite(ePin, HIGH);
-      digitalWrite(fPin, HIGH);
-      digitalWrite(gPin, HIGH);
-      break;
-
-    case 8:
-      digitalWrite(aPin, LOW);
-      digitalWrite(bPin, LOW);
-      digitalWrite(cPin, LOW);
-      digitalWrite(dPin, LOW);
-      digitalWrite(ePin, LOW);
-      digitalWrite(fPin, LOW);
-      digitalWrite(gPin, LOW);
-      break;
-
-    case 9:
-      digitalWrite(aPin, LOW);
-      digitalWrite(bPin, LOW);
-      digitalWrite(cPin, LOW);
-      digitalWrite(dPin, LOW);
-      digitalWrite(ePin, HIGH);
-      digitalWrite(fPin, LOW);
-      digitalWrite(gPin, LOW);
-      break;
-  }
-}
-
-// Multiplexing
-void showDigits(){
-
-  digitalWrite(dig1Pin, HIGH);
-  displayNumber(dig1);
-  delay(4);
-  digitalWrite(dig1Pin, LOW);
-
-  digitalWrite(dig2Pin, HIGH);
-  displayNumber(dig2);
-  delay(4);
-  digitalWrite(dig2Pin, LOW);
-
-  digitalWrite(dig3Pin, HIGH);
-  displayNumber(dig3);
-  delay(4);
-  digitalWrite(dig3Pin, LOW);
-
-  digitalWrite(dig4Pin, HIGH);
-  displayNumber(dig4);
-  delay(4);
-  digitalWrite(dig4Pin, LOW);
-}
-
-// Counter
-void incrementCounter(){
-
-  dig1++;
-
-  if(dig1 >= 10){
-    dig1 = 0;
-    dig2++;
-  }
-
-  if(dig2 >= 10){
-    dig2 = 0;
-    dig3++;
-  }
-
-  if(dig3 >= 10){
-    dig3 = 0;
-    dig4++;
-  }
-
-  if(dig4 >= 10){
-    dig4 = 0;
-  }
-}
-
-// LCD update
-void updateLCD(){
-
-  lcd.setCursor(0,1);
-
-  lcd.print(dig4);
-  lcd.print(dig3);
-  lcd.print(dig2);
-  lcd.print(dig1);
-  lcd.print(" ");
 }
 
 void loop() {
 
-  for(int i=0; i<50; i++){
-    showDigits();
-  }
+ unsigned long now = millis();
 
-  incrementCounter();
+ if(now-lastMuxTime>=3){
 
-  updateLCD();
+ lastMuxTime=now;
 
-  delay(1000);
+ refreshDisplay();
+
+ }
+
+ if(now-lastCountTime>=1000){
+
+ lastCountTime=now;
+
+ counter++;
+
+ if(counter>9999)
+ counter=0;
+
+ updateLCD();
+
+ }
+
 }
